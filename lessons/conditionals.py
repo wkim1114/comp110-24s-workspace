9780727ed17e9221e89f15d259e26a03ec47eeb5
Will Kim <wkim1114@unc.edu>
2024-01-21 09:50:51 -0500

    VS00 conditionals

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -5,6 +5,16 @@ print(type(user_input))
 user_number: int = int(user_input)
 print(type(user_number))
 
-# 
+# if user_number is less than 10, print "small"
+if user_number < 10:
+    print("small")
+else: # user_number >= 10
+    print("big")
 
-print(user_input)
+# if user_number is even, print "even"
+if user_number % 2 ==0:
+    print("even")
+else: # user_number is odd
+    print("odd")
+
+print(user_number)
