18bfcbd081e5f1ff1fa51ae9f3a7e4a6ebf0c1fc
Will Kim <wkim1114@unc.edu>
2024-01-21 09:43:18 -0500

    VS00 Conditionals

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -1,2 +1,10 @@
 """Demonstrate behavior of conditionals"""
 
+user_input: str = input("Pick a number: ")
+print(type(user_input))
+user_number: int = int(user_input)
+print(type(user_number))
+
+# 
+
+print(user_input)
