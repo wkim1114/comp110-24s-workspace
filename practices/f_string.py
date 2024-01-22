53e42421b8f9889186948dd153dbd113d9dcac8b
Will Kim <wkim1114@unc.edu>
2024-01-22 15:30:31 -0500

    f_string practices

---
+++ practices/f_string.py
@@ -0,0 +1,8 @@
+"""Practicing f string"""
+
+course: int = 110
+print("I am in COMP" + str(course) + " right now!")
+
+print(f"I am in COMP{course} right now!")
+
+print("I am in COMP" + 110 + " right now!")
